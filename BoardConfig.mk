d4513ff73a9fcfe0df17afaaf7b7e57ab727af65
Vatsal <koradiavatsal@gmail.com>
2016-06-10 18:48:32 +0530

    4.9
    Change-Id: Iec83200afb50f442ad87c0a61894527410f1d1e5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,11 +39,11 @@ ENABLE_GCCONLY := true
 # Kernel
 TARGET_KERNEL_CONFIG := titan_defconfig
 
-#Kernel Toolchain
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-5.3-uber/bin
+# Kernel Toolchain
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.9-uber/bin
 KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
 # Rom Toolchain
-#TARGET_GCC_VERSION_EXP := 5.3-uber
+TARGET_GCC_VERSION_EXP := 4.9-uber
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 10444800
