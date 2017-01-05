4f8e82fbc11f26bdf2579951bd0e92fcdafe2ba8
Vatsal <koradiavatsal@gmail.com>
2017-01-05 13:09:48 +0530

    Not needed
    Change-Id: I06bc8c94a85ce132dacdffa59d5a272e223ac127

--- tipsy.mk
+++ tipsy.mk
@@ -2,7 +2,6 @@ $(call inherit-product, device/motorola/titan/full_titan.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/tipsy/config/common_full_phone.mk)
-$(call inherit-product, vendor/tipsy/config/caf_fw.mk)
  
 PRODUCT_RELEASE_NAME := titan
 PRODUCT_NAME := tipsy_titan
